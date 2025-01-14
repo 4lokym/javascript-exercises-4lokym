2a083681b7b4dc595973d63ed95524d581a7224a
4lokym <mykola.koshilko@gmail.com>
2025-01-14 16:51:14 +0100

    Exercise 03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reversedWord = "";
 
+    for(let wordLenght = word.length - 1; wordLenght >= 0; wordLenght--){
+        reversedWord += word.at(wordLenght);
+    }
+    return reversedWord;
 };
 
 // Do not edit below this line
