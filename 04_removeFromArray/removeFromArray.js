bc0cd4f74d5d8d52815542e79306ea69630f0df2
4lokym <mykola.koshilko@gmail.com>
2025-01-14 16:50:16 +0100

    Exercise 04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+
+    for(let i = 1; i<arguments.length; i++){
+        array = rmElements(array, arguments[i]);
+    }
+
+    return array;
 };
+
+function rmElements(array, element){
+    let newArray = [];
+    for(let i=0; i<array.length; i++){
+        if(array[i] === element){
+            continue;
+        }
+
+        newArray.push(array[i]);
+    }
+
+    return newArray;
+}
+
+console.log(removeFromArray([1,2,3,4], 3));
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
