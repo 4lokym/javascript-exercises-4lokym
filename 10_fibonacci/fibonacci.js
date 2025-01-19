f5c5e563600229d1b29400585cc47b34b936b458
4lokym <mykola.koshilko@gmail.com>
2025-01-19 13:00:47 +0100

    Exercise 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let a = 1;
+    let b = 1;
+    let temp = 0;
+    
+    num = Number.parseInt(num);
 
+    if(num<0){
+        return "OOPS";
+    }else if(num === 0){
+        return 0;
+    }else{
+        for(let i = 0; i< num-2; i++){
+            temp = a+b;
+            b = a;
+            a = temp;
+        }
+    }  
+    return a;
 };
 
 // Do not edit below this line
