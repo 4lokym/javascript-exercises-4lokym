916bc066e1b231fc1a0b5841c94b75cfd2c978df
4lokym <mykola.koshilko@gmail.com>
2025-01-19 14:17:06 +0100

    Exercise 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
 
+    obj.sort((item1, item2) =>{
+        death1 = item1.yearOfDeath;
+        death2 = item2.yearOfDeath;
+        
+        
+        if(item2.yearOfDeath === undefined){
+            death1 = (new Date().getFullYear());
+            console.log(death1);
+        }
+        if(item2.yearOfDeath === undefined){
+            death2 = (new Date().getFullYear());
+        }
+
+        return (death2 - item2.yearOfBirth) - (death1 - item1.yearOfBirth);
+    });
+    return obj[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
