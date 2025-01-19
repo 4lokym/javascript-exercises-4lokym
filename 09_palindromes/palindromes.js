ebd48134331ad9c7a237c787ade50c532952230e
4lokym <mykola.koshilko@gmail.com>
2025-01-19 12:18:14 +0100

    Exercise 09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+function simplePalindrome(str){
+    if(str.length == 0){
+        return true;
+    }else if(str.length == 1){
+        return true;
+    }else{
+        return (str[0] === str[str.length-1]) && simplePalindrome(str.slice(1, str.length-1));
+    }
+}
 
+function palindromes(str) {
+    const punctuation = "!\"\\£$%&/()=?'^|;,:.-_#@[]{}*<> ";
+    
+    let newString = "";
+
+    for(let i = 0; i < str.length; i++){
+        let chara = str.charAt(i);
+        if(!(punctuation.includes(chara))){
+            newString += chara;
+        }
+    }
+    return simplePalindrome(newString.toLowerCase());
 };
 
-// Do not edit below this line
+console.log(palindromes("ZZZZ car, a man, a maracaz."));
+
+
+ //Do not edit below this line
 module.exports = palindromes;
