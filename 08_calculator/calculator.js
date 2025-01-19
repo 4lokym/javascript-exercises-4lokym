7aa5faaf5d1ceff053cbeb4e04cbb5bedda29b1b
4lokym <mykola.koshilko@gmail.com>
2025-01-19 11:16:07 +0100

    Exercise 08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(addend1, addend2) {
+	return addend1 + addend2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce(function(sum, item){
+    return sum += item;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce(function(mult, item){
+    return mult *= item;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+
+  for(let i=1; i<=num; i++){
+    result *= i
+  }
+  return result;
 };
 
 // Do not edit below this line
